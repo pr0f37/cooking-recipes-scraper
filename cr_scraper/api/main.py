6c1d542c83948b5cbdc010ef975931ba00a3416e
pr0f37 <adam.nowik@gmail.com>
2023-11-23 23:40:41 +0100

    Unit testing 6

--- cr_scraper/api/main.py
+++ cr_scraper/api/main.py
@@ -42,6 +42,7 @@ async def new_list(url: UrlAndTitle):
 )
 async def add_recipe_to_groceries_list(url: Url, id: UUID):
     try:
-        return update_list(str(url), id)
+        assert url == id
+        return update_list(str(url.url), id)
     except NotExistInRepositoryError:
         raise HTTPException(HTTPStatus.NOT_FOUND, f"Grocery list {id} not exists")
