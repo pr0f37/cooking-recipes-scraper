e6405e5e1f639d35e41736ea9f34475f26f414c5
pr0f37 <adam.nowik@gmail.com>
2024-01-22 23:36:46 +0100

    progress bar, polling

--- cr_scraper/api/main.py
+++ cr_scraper/api/main.py
@@ -3,7 +3,7 @@ from time import sleep
 from uuid import UUID
 
 from fastapi import FastAPI, HTTPException, Request, Response
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.responses import FileResponse, HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import ValidationError
@@ -19,6 +19,7 @@ from cr_scraper.api.services.recipes import (
     update_list,
     update_list_add_recipe,
 )
+from cr_scraper.archiver import Archiver
 from cr_scraper.grocery_list.model import GroceryList
 from cr_scraper.persistence.repository import NotExistInRepositoryError
 from cr_scraper.scraper.model import Recipe
@@ -75,6 +76,7 @@ async def show_grocery_lists(request: Request, q: str | None = None, page: int =
             "grocery_lists": grocery_lists,
             "errors": errors,
             "page": page,
+            "archiver": Archiver.get(),
         },
     )
 
@@ -116,6 +118,21 @@ async def new_list_html(request: Request):
 
     initialize_list(str(url.url), url.title)
     return RedirectResponse("/grocery_lists/html", status_code=HTTPStatus.SEE_OTHER)
+
+
+@app.get("/grocery_lists/archive/html")
+async def archive_status(request: Request):
+    archiver = Archiver.get()
+    return templates.TemplateResponse(
+        name="grocery_list/archive_ui.html",
+        context={"request": request, "archiver": archiver},
+    )
+
+
+@app.get("/grocery_lists/archive/file")
+async def archive_content(request: Request):
+    manager = Archiver.get()
+    return FileResponse(manager.archive_file())
 
 
 @app.get(
@@ -238,3 +255,13 @@ async def delete_all_grocery_lists_html(request: Request):
     for id in list_ids:
         delete_list(id)
     return RedirectResponse("/grocery_lists/html", status_code=HTTPStatus.SEE_OTHER)
+
+
+@app.post("/grocery_lists/archive/html", response_class=HTMLResponse)
+async def start_archive(request: Request):
+    archiver = Archiver.get()
+    archiver.run()
+    return templates.TemplateResponse(
+        name="grocery_list/archive_ui.html",
+        context={"request": request, "archiver": archiver},
+    )
